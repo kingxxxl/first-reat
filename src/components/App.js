7039cf3757cc991054fc8c096a77c672ed1b9145
kingxxxl <103191276+kingxxxl@users.noreply.github.com>
2022-06-21 09:34:46 +0300

    +props

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
 import '../App.css';
 import Cat from "./cat";
 
+const img1 = 'https://images.all-free-download.com/images/graphicwebp/cat_small_cat_cats_eye_214515.webp';
+const img2 = 'https://images.all-free-download.com/images/graphicwebp/cat_cat_face_cats_eyes_240527.webp';
+const img3 = 'https://images.all-free-download.com/images/graphicwebp/hangover_cat_breed_cat_237507.webp';
+const img4 = 'https://images.all-free-download.com/images/graphicwebp/cat_cat_face_cats_eyes_236797.webp';
+
+
 function App() {
-    Cat('../../public/ca1.jpeg', 'Caty', '2')
+
     return (
-    <Cat/>
-    )
-        ;
+        <div className="App">
+            <Cat age={'2'}
+                 img={img1}/>
+
+            <Cat name={'Caty'} age={'2'}
+                 img={img2}/>
+
+            <Cat name={'Caty2'} age={'4'}
+                 img={img3}/>
+
+            <Cat name={'Caty3'} age={'5'}
+                 img={img4}/>
+        </div>
+
+    );
 }
 
 export default App;
--- src/components/cat.js
+++ src/components/cat.js
@@ -1,25 +1,13 @@
-function Cat() {
+function Cat({img, name, age}) {
+    if (name == undefined) {
+        name = "You have to provide a name"
+    }
     return (
 
         <div>
-            <img src={'https://images.all-free-download.com/images/graphicwebp/cat_small_cat_cats_eye_214515.webp'}/>
-            <p>Name: {'name: caty'}</p>
-            <p>Age: {'age: 2'}</p>
-
-
-            <img src={'https://images.all-free-download.com/images/graphicwebp/cat_cat_face_cats_eyes_240527.webp'}/>
-            <p>Name: {'name: caty'}</p>
-            <p>Age: {'age: 3'}</p>
-
-
-            <img src={'https://images.all-free-download.com/images/graphicwebp/hangover_cat_breed_cat_237507.webp'}/>
-            <p>Name: {'name: caty'}</p>
-            <p>Age: {'age: 4'}</p>
-
-
-            <img src={'https://images.all-free-download.com/images/graphicwebp/cat_cat_face_cats_eyes_236797.webp'}/>
-            <p>Name: {'name: caty'}</p>
-            <p>Age: {'age: 5'}</p>
+            <img src={img} alt="img-cat"/>
+            <p>Name: {name}</p>
+            <p>Age: {age}</p>
         </div>
 
 
