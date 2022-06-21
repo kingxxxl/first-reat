ffef5e9474344308bf686eb3b987f530afd52f4f
kingxxxl <103191276+kingxxxl@users.noreply.github.com>
2022-06-21 08:44:01 +0300

    hw1 react

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../App.css';
+import Cat from "./cat";
+
+function App() {
+    Cat('../../public/ca1.jpeg', 'Caty', '2')
+    return (
+    <Cat/>
+    )
+        ;
+}
+
+export default App;
---
+++ src/components/cat.js
@@ -0,0 +1,29 @@
+function Cat() {
+    return (
+
+        <div>
+            <img src={'https://images.all-free-download.com/images/graphicwebp/cat_small_cat_cats_eye_214515.webp'}/>
+            <p>Name: {'name: caty'}</p>
+            <p>Age: {'age: 2'}</p>
+
+
+            <img src={'https://images.all-free-download.com/images/graphicwebp/cat_cat_face_cats_eyes_240527.webp'}/>
+            <p>Name: {'name: caty'}</p>
+            <p>Age: {'age: 3'}</p>
+
+
+            <img src={'https://images.all-free-download.com/images/graphicwebp/hangover_cat_breed_cat_237507.webp'}/>
+            <p>Name: {'name: caty'}</p>
+            <p>Age: {'age: 4'}</p>
+
+
+            <img src={'https://images.all-free-download.com/images/graphicwebp/cat_cat_face_cats_eyes_236797.webp'}/>
+            <p>Name: {'name: caty'}</p>
+            <p>Age: {'age: 5'}</p>
+        </div>
+
+
+    );
+}
+
+export default Cat;
